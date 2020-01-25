22c0cbe2ccb72813d36b5f4e6ece6f625ff138b5
Nick Pateman <nickj@devoctomy.com>
2020-01-25 09:08:14 +0000

    Initial commit

---
+++ Octopanel-POC/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using Octopanel_POC.ViewModels;
+using Octopanel_POC.Views;
+
+namespace Octopanel_POC
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
